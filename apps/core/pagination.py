a6e51f0014a0bd0c14d7b594daecc79e9235796c
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-07-11 16:50:41 +0300

    added custom pagination.

---
+++ apps/core/pagination.py
@@ -0,0 +1,20 @@
+from rest_framework import pagination
+from rest_framework.response import Response
+from rest_framework.utils.urls import replace_query_param
+
+
+class CustomPagination(pagination.PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'last_page': self.get_last_link(),
+            'page_number': self.get_page_number(self.request, None),
+            'count': self.page.paginator.count,
+            'results': data
+        })
+
+    def get_last_link(self):
+        url = self.request.build_absolute_uri()
+        page_number = self.page.paginator.num_pages
+        return replace_query_param(url, self.page_query_param, page_number)
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     # apps
     'apps.posts',
+    'apps.core',
     # third party apps
     'corsheaders',
     'rest_framework',
@@ -151,7 +152,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':(
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'apps.core.pagination.CustomPagination',
     'PAGE_SIZE': 4
 }
 
