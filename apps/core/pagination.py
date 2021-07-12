83356ec4ed925dbd4aef17a90c7ac02c06a90bd6
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-07-12 20:44:29 +0300

    little more pagination

--- apps/core/pagination.py
+++ apps/core/pagination.py
@@ -8,13 +8,19 @@ class CustomPagination(pagination.PageNumberPagination):
         return Response({
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            'last_page': self.get_last_link(),
-            'page_number': self.get_page_number(self.request, None),
+            'first_page':self.get_first_page_link(),
+            'last_page': self.get_last_page_link(),
+            'page_number': int(self.get_page_number(self.request, None)),
             'count': self.page.paginator.count,
             'results': data
         })
 
-    def get_last_link(self):
+    def get_last_page_link(self):
         url = self.request.build_absolute_uri()
         page_number = self.page.paginator.num_pages
+        return replace_query_param(url, self.page_query_param, page_number)
+
+    def get_first_page_link(self):
+        url = self.request.build_absolute_uri()
+        page_number = 1
         return replace_query_param(url, self.page_query_param, page_number)
